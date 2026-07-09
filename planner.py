035108b8896d832c43556570032cf4ca84e2968d
sequenc <skip2025nws@gmail.com>
2026-07-09 13:45:55 +0530

    Implement Planner class with AI decision-making

---
+++ planner.py
@@ -0,0 +1,37 @@
+from groq import Groq
+
+
+class Planner:
+
+    def __init__(self, api_key):
+        self.client = Groq(api_key=api_key)
+
+    def decide(self, query):
+
+        prompt = f"""
+You are an AI agent.
+
+Available tools:
+1. calculate(expression) -> Performs mathematical calculations.
+
+If the user needs calculation, respond ONLY in this format:
+
+TOOL: calculate
+INPUT: <expression>
+
+Otherwise respond ONLY:
+
+TOOL: none
+INPUT: none
+
+User: {query}
+"""
+
+        response = self.client.chat.completions.create(
+            model="llama-3.3-70b-versatile",
+            messages=[
+                {"role": "user", "content": prompt}
+            ]
+        )
+
+        return response.choices[0].message.content
